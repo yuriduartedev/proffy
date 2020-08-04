7f7859aad9ba71bb2121af337fcb583506e667aa
Yuri Duarte <yuriduartedev@gmail.com>
2020-08-04 13:23:20 -0300

    Finaliza rotade criação de aulas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) => {
-
-  response.json({ message: 'Hello' })
-});
 
 app.listen(3333);
