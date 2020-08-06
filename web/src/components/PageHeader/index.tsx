cba750facec0cd11da9ac303e4de561373d7ea4f
Yuri Duarte <yuriduartedev@gmail.com>
2020-08-06 16:40:13 -0300

    Finaliza telas do frontend

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,9 +8,10 @@ import backIcon from '../../assets/images/icons/back.svg';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ children, title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ children, title, description }) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -22,7 +23,9 @@ const PageHeader: React.FC<PageHeaderProps> = ({ children, title }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
-        {children}
+        { description && <p>{description}</p> }
+
+        { children }
       </div>
 
     </header>
