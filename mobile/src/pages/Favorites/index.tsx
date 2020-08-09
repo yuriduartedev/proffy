c106538333d5686628e5fe1eac1e03277b403f03
Yuri Duarte <yuriduartedev@gmail.com>
2020-08-09 12:39:07 -0300

    Criação de tela TeacherList e Favorites

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 const Favorites: React.FC = () => {
   return (
-    <View style={styles.container} />
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <PageHeader title="Meus Proffys favoritos" />
+      </View>
+    </View>
   )
 }
 
